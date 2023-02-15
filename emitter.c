a85df5c93fe9687fd9381fc9feafedd0908496e0
Aaron Lo <22aaronl@students.harker.org>
2023-02-15 01:15:28 -0600

    warning

--- emitter.c
+++ emitter.c
@@ -68,7 +68,7 @@ void push_register(emitter_t *emitter, char *name)
         {
             emitter->emit_instruction->pop = false;
             emitter->emit_instruction->reg = name;
-            printf("pushq %%%s", name);
+            printf("pushq %%%s", name); //TODO: WARNING THIS WIILL RUN INTO MAJOR ISSUES ON NOT STACK COMPILER
         }
     }
     else
