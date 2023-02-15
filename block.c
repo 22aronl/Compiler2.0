8c3173ebfda606bb1458ae6093a892f71891b6ea
Aaron Lo <22aaronl@students.harker.org>
2023-02-15 04:05:18 -0600

    expression adding

--- block.c
+++ block.c
@@ -373,7 +373,7 @@ void create_next_use_information(block_t *block, struct map *map, struct queue_h
     block->variables_size = 2;
     block->variables_index = 0;
 
-    if (block->has_jump && !block->unconditional_jump)
+    if (block->has_jump && !block->unconditional_jump || block->is_while)
     {
         next_use_expression(block->jump_expression, block, block->statement_size, queue);
     }
